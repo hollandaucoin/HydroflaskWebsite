e1a34d46b4d7a1ec4c40bc8477e13787a33a89e4
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:47:50 -0800

    HydroflaskWebsite

--- pages/addProductPage.php
+++ pages/addProductPage.php
@@ -1,6 +1,6 @@
 <!--
 
-@author Holland Aucoin and Andrei Yanovich
+@author Holland Aucoin
 @name Hydroflask Website
 @desc addProductPage - This is a page that allows an admin user to add a product to the website
 
