f30022338749a94e2e66bace33b9ede611913028
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:42:49 -0800

    HydroflaskWebsite

--- classes/data/discountDataService.php
+++ classes/data/discountDataService.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc discountDataService - This is a class to perform the persistence logic on the discount table in the database
  */
