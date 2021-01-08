e1c622de6ca59577da53807d75a70bdbe0ed0d03
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:43:06 -0800

    HydroflaskWebsite

--- classes/data/lidDataService.php
+++ classes/data/lidDataService.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc lidDataService - This is a class to perform the persistence logic of lids to the database
  */
