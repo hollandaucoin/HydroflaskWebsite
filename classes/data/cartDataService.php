2ccaeb9393241193a17836c80b3c96a662af18b4
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:41:55 -0800

    HydroflaskWebsite

--- classes/data/cartDataService.php
+++ classes/data/cartDataService.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc cartDataService - This is a class to perform the persistence logic on the cart table in the database
  */
