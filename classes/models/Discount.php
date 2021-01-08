f2c86c8a1cfe26ee22686059efd4d9d6603b33ca
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:35:15 -0800

    HydroflaskWebsite

--- classes/models/Discount.php
+++ classes/models/Discount.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc discount - This is a class that acts as a discount object
  */
