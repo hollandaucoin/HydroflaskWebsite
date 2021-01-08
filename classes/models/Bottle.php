e3d11438b95649c5d14a2aa4691250bd8da49428
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:34:40 -0800

    HydroflaskWebsite

--- classes/models/Bottle.php
+++ classes/models/Bottle.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc Bottle - This is a class that acts as a bottle object
  */
