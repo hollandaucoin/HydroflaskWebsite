8f4b72833d0497a7c42a7d2def4bf4289f60e35f
Holland Aucoin <holland.aucoin2@gmail.com>
2021-01-07 19:40:59 -0800

    HydroflaskWebsite

--- classes/data/addressDataService.php
+++ classes/data/addressDataService.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Holland Aucoin and Andrei Yanovich
+ * @author Holland Aucoin
  * @name Hydroflask Website
  * @desc addressDataService - This is a class to perform the persistence logic on the address table in the database
  */
